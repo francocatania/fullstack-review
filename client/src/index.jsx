c7725f34945cb330b5450a59e3fe331824ccd009
Franco <frc.catani@gmail.com>
2018-01-03 15:34:22 -0800

    search posts in db and gets repos

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
+import RepoVisualizer from './components/RepoVisualizer.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,17 +14,38 @@ class App extends React.Component {
     
   }
 
-  search (term) {
-    console.log(`${term} was searched`);
-    let myPost = new Request('http://127.0.0.1:1128/repos', {method: 'POST', body: {username: term});
+  search (username) {
+    console.log(`${username} was searched`);
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    let myPost = new Request('http://127.0.0.1:1128/repos', {method: 'POST', headers: myHeaders, body: JSON.stringify({username: username})});
     fetch(myPost)
       .then(response => {
+        console.log('Posting');
         if (response.ok) {
+          console.log('Posted!');
           return response.json();
         }  
       })
       .then(json => {
-        this.setState({repos: json});
+        if (json === 'Successfully Posted') {
+          var myHeaders = new Headers();
+          var myInit = { 
+            method: 'GET',
+            headers: myHeaders,
+            mode: 'cors',
+            cache: 'default' };
+          fetch(`/repos/${username}`, myInit)
+            .then(response => {
+              if (response.ok) {
+                return response.json();
+              }
+            })
+            .then(json => {
+              console.log(json);
+              this.setState({repos: json}, () => {console.log(this.state.repos)});
+            })
+        }
       })
   }
 
@@ -32,6 +54,11 @@ class App extends React.Component {
       <h1>Github Fetcher</h1>
       <RepoList repos={this.state.repos}/>
       <Search onSearch={this.search.bind(this)}/>
+      
+      {this.state.repos.map(repo => {
+        return <RepoVisualizer key={repo.repo_id} repo_name={repo.repo_name} owner={repo.owner} />
+      })}
+
     </div>)
   }
 }
